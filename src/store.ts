609817ac0cf48771f83bcda8e172672ce00f6852
giyun-cloud <nangiyoon@gmail.com>
2021-09-14 11:17:04 +0900

    safe global state - 1

---
+++ src/store.ts
@@ -0,0 +1,54 @@
+import { NewsFeed } from "./types";
+
+class Store {
+  feeds: NewsFeed[];
+  _currentPage: number;
+
+  constructor() {
+    this.feeds = [];
+    this._currentPage = 1;
+  }
+
+  get currentPage(): number {
+    return this._currentPage
+  }
+
+  set currentPage(page: number) {
+    this._currentPage = page;
+  }
+
+  get nextPage(): number {
+    return this._currentPage + 1;
+  }
+
+  get prevPage(): number {
+    return this._currentPage - 1 < 1 ? 1 : this._currentPage -1;
+  }
+
+  get numberOfFeeds(): number {
+    return this.feeds.length;
+  }
+
+  get hasFeeds(): boolean {
+    return this.feeds.length > 0;
+  }
+
+  getAllFeeds(): NewsFeed[] {
+    return this.feeds
+  }
+
+  getFeed(position: number): NewsFeed {
+    return this.feeds[position]
+  }
+
+  sefFeeds(feeds: NewsFeed[]): void {
+    this.feeds = feeds.map(feed => ({
+      ...feed,
+      read: false,
+    }))
+  }
+
+  makeRead(id: number): void {
+    this.feeds.filter(feed => feed.id === id)[0].read = true;
+  }
+}
